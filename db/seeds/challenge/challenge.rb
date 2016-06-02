ddc1a1dacc65d127df52dcb0c82b66427b0ab340
Jason Black <makotothedragon@gmail.com>
2016-06-02 17:02:48 -0600

    Hotfix:  Adjust field for Challenge to ensure db:recreate works properly

--- db/seeds/challenge/challenge.rb
+++ db/seeds/challenge/challenge.rb
@@ -7,7 +7,7 @@ challenge = Challenge.create!(title: 'The First Challenge',
                               background: 'School systems across the country are investing time and money to create and share professional development videos, but are these videos giving educators what they need, when they need it? What if teachers came together to share their experiences and come up with ideas for how videos can better meet their professional learning needs?',
                               outcome: 'The First Challenge is a test challenge.',
                               help: "There are four ways you can get involved over the course of the Challenge: share your experiences, contribute your ideas for how to improve classroom video, develop approaches, and try out popular solutions in your classroom. You'll eventually be able to participate in any stage. Scroll down to learn more, or click \"Get Involved\" to start now. Welcome!",
-                              cta_tackle: 'Want to help?',
+                              cta: 'Want to help?',
                               starts_at: Time.now,
                               ends_at: 1.month.from_now,
                               featured: true
