2d2478b5a9d025254608d51c0dc9264eef507409
Jason Black <makotothedragon@gmail.com>
2016-06-03 10:41:57 -0600

    Localize parts of the user pages

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -11,7 +11,7 @@ module UsersHelper
   def submissions(profile_user, viewing_user)
     viewing_user = nil unless viewing_user == profile_user
     (
-      profile_user.experiences.published(viewing_user) +
+    profile_user.experiences.published(viewing_user) +
       profile_user.ideas.published(viewing_user) +
       profile_user.recipes.published(viewing_user) +
       profile_user.solutions.published(viewing_user) +
@@ -27,7 +27,9 @@ module UsersHelper
       names.to_sentence
     else
       remainder = names.size - visible_name_size
-      "#{names.first(visible_name_size).join(', ')} and #{pluralize(remainder, 'other')}"
+      I18n.t('users.cards._submission.pluralized_name_list',
+             names: names.first(visible_name_size).join(', '),
+             remainder: pluralize(remainder, I18n.t('users.cards._submission.other')))
     end
   end
 
